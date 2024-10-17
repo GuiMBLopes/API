fce8d091e1e3ff6b9de929a2fcfb64fef8e5f507
GabrielRBSilva <gabriel.rbs14@gmail.com>
2024-10-17 16:02:15 -0300

    repository e service

--- FinalAPI/src/main/java/br/org/serratec/FinalAPI/service/UsuarioService.java
+++ FinalAPI/src/main/java/br/org/serratec/FinalAPI/service/UsuarioService.java
@@ -1,5 +1,14 @@
 package br.org.serratec.FinalAPI.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.org.serratec.FinalAPI.repository.UsuarioRepository;
+
+@Service
 public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 }
